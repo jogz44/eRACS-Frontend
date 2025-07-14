f22670f90cfe649771965a98d9c7e512cd43f113
Michael June Llano <m.llano.140746.tc@umindanao.edu.ph>
2025-07-14 14:53:01 +0800

    back to center the loginLayout

--- eRAC-frontend-main/src/layouts/LoginLayout.vue
+++ eRAC-frontend-main/src/layouts/LoginLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <q-layout view="lHh Lpr lFf">
     <div class="bg-wrapper">
-      <q-page-container class="flex-center">
+      <q-page-container class="flex flex-center">
         <router-view />
       </q-page-container>
     </div>
