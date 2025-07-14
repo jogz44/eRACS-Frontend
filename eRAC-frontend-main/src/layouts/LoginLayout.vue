c4921f475131b54b1c055a760e6eacdd78af2717
Michael June Llano <m.llano.140746.tc@umindanao.edu.ph>
2025-07-14 11:02:16 +0800

    back to original blur

--- eRAC-frontend-main/src/layouts/LoginLayout.vue
+++ eRAC-frontend-main/src/layouts/LoginLayout.vue
@@ -32,7 +32,7 @@ export default {
   bottom: 0;
   background: url('src/assets/cityhall.jpg') no-repeat center center;
   background-size: cover;
-  filter: blur(100px);
+  filter: blur(4px);
   z-index: 0;
 }
 
