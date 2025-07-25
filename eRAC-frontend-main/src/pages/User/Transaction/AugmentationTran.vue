523a776acff2786061edde27bef81e7f3ad40ef0
Jan Paul <balinojanpaul@gmail.com>
2025-07-25 15:08:02 +0800

    done fixing the Current Transactions responsiveness

--- eRAC-frontend-main/src/pages/User/Transaction/AugmentationTran.vue
+++ eRAC-frontend-main/src/pages/User/Transaction/AugmentationTran.vue
@@ -37,4 +37,18 @@ import AugExpenseDetailDial from 'components/augmentation/AugExpenseDetailDial.v
   background-color: #D9D9D9; /* Light gray background */
   min-height: 100vh; /* Ensure full height */
 }
+
+@media (min-width: 1025px) {
+  .custom-search-input {
+    width: 300px !important;
+    min-width: 0 !important;
+    max-width: 600px !important;
+  }
+  .custom-date-from,
+  .custom-date-to {
+    width: 180px !important;
+    min-width: 0 !important;
+    max-width: 250px !important;
+  }
+}
 </style>
