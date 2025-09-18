0649b508fa6fb1a01c56b46ec84820ab32d4c8e4
Jan Paul <balinojanpaul@gmail.com>
2025-09-18 16:06:58 +0800

    Update axios baseURL to use the correct API endpoint

--- eRAC-frontend-main/src/boot/axios.js
+++ eRAC-frontend-main/src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:8000',
+  baseURL: process.env.API_URL || 'http://192.168.150.135:8000',
   //baseURL: process.env.API_URL || 'http://192.168.150.161:8000',
   withCredentials: true,
   headers: {
